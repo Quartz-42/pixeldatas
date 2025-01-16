bc8d999d1981f83bbd190ad24b3898e751c4e002
Quartz-42 <benjamin.baroche@free.fr>
2025-01-16 21:06:00 +0100

    Simplification methode pour trouver les random Pokemons

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Repository\PokemonRepository;
-use App\Repository\TypeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -16,22 +15,8 @@ class HomeController extends AbstractController
         // Récupérer 3 Pokémon aléatoires
         $randomPokemons = $pokemonRepository->getRandomPokemons(3);
 
-        // Préparer un tableau contenant les Pokémon avec leurs types
-        $pokemonData = [];
-        foreach ($randomPokemons as $pokemon) {
-            $types = [];
-            foreach ($pokemon->getTypes() as $type) {
-                $types['name'] = $type->getName();
-                $types['image'] = $type->getImage();
-            }
-            $pokemonData[] = [
-                'pokemon' => $pokemon,
-                'types' => $types,
-            ];
-        }
-
         return $this->render('home/index.html.twig', [
-            'randomPokemons' => $pokemonData,
+            'randomPokemons' => $randomPokemons,
         ]);
     }
 
