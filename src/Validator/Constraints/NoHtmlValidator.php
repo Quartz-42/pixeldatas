2f7b5503f6b23ae8d8961af43ebbc6c01152efcd
Quartz-42 <benjamin.baroche@free.fr>
2025-06-01 15:16:44 +0200

    Update to symfony 7.3 and correct type of multiple objects

--- src/Validator/Constraints/NoHtmlValidator.php
+++ src/Validator/Constraints/NoHtmlValidator.php
@@ -7,7 +7,7 @@ use Symfony\Component\Validator\ConstraintValidator;
 
 class NoHtmlValidator extends ConstraintValidator
 {
-    public function validate($value, Constraint $constraint)
+    public function validate($value, Constraint $constraint): void
     {
         if (preg_match('/<[^>]+>/', $value)) {
             $this->context->buildViolation($constraint->message)
