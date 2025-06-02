c84795920f74ebb2e503d5190bf8d8c2c5a81a67
Quartz-42 <benjamin.baroche@free.fr>
2025-06-02 10:56:43 +0200

    Change CSS and navbar

--- src/Controller/PokemonController.php
+++ src/Controller/PokemonController.php
@@ -7,6 +7,8 @@ use App\Repository\PokevolutionRepository;
 use Pagerfanta\Doctrine\ORM\QueryAdapter;
 use Pagerfanta\Pagerfanta;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\BrowserKit\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 use Symfony\Component\Routing\Attribute\Route;
@@ -14,6 +16,14 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('/pokemons')]
 class PokemonController extends AbstractController
 {
+    public function __construct(
+        private RequestStack $requestStack,
+    ) {
+        // Accessing the session in the constructor is *NOT* recommended, since
+        // it might not be accessible yet or lead to unwanted side-effects
+        // $this->session = $requestStack->getSession();
+    }
+
     #[Route('/list', name: 'app_all_pokemon')]
     public function showAllPokemons(
         PokemonRepository $pokemonRepository,
@@ -29,6 +39,10 @@ class PokemonController extends AbstractController
         // Calculer les pages visibles
         $visiblePages = $this->getVisiblePages($pager);
 
+        //recupérer la page en cours et stocker en session
+        $session = $this->requestStack->getSession();
+        $session->set('current_page', $pager->getCurrentPage());
+
         $pokemonTypes = $pokemonRepository->findPokemonTypes();
         $generations = $pokemonRepository->findPokemonGenerations();
 
@@ -38,30 +52,6 @@ class PokemonController extends AbstractController
             'pokemonTypes' => $pokemonTypes,
             'generations' => $generations,
         ]);
-    }
-
-    private function getVisiblePages(Pagerfanta $pager): array
-    {
-        $currentPage = $pager->getCurrentPage();
-        $nbPages = $pager->getNbPages();
-
-        $pages = [];
-
-        // Toujours afficher la première page
-        $pages[] = 1;
-
-        // Pages autour de la page actuelle (3 avant et 3 après)
-        for ($i = max(2, $currentPage - 3); $i <= min($nbPages - 1, $currentPage + 3); ++$i) {
-            $pages[] = $i;
-        }
-
-        // Toujours afficher la dernière page
-        if ($nbPages > 1) {
-            $pages[] = $nbPages;
-        }
-
-        // Éliminer les doublons et maintenir l'ordre
-        return array_values(array_unique($pages));
     }
 
     #[Route('/details/{name}', name: 'app_pokemon_details')]
@@ -75,9 +65,13 @@ class PokemonController extends AbstractController
         // Récupérer les évolutions du Pokémon
         $evolutions = $pokevolutionRepository->findOneBy(['pokemon' => $pokemon->getId()]);
 
+        //recuperer la page en cours
+        $currentPage = $this->getCurrentPage();
+
         return $this->render('pokemon/show_details.html.twig', [
             'pokemon' => $pokemon,
             'evolutions' => $evolutions,
+            'currentPage' => $currentPage,
         ]);
     }
 
@@ -120,6 +114,9 @@ class PokemonController extends AbstractController
             40
         );
 
+        //recuperer la page en cours
+        $currentPage = $this->getCurrentPage();
+
         $numberOfPokemons = count($pokemonRepository->getPokemonsByType($type));
 
         // Calculer les pages visibles
@@ -130,14 +127,50 @@ class PokemonController extends AbstractController
             'visiblePages' => $visiblePages,
             'type' => $type,
             'numberOfPokemons' => $numberOfPokemons,
+            'currentPage' => $currentPage,
         ]);
     }
 
     #[Route('/{type}/card', name: 'app_type_show_card', methods: ['GET'])]
     public function showCard(string $type): Response
     {
+        //recuperer la page en cours
+        $currentPage = $this->getCurrentPage();
+
         return $this->render('pokemon/_card.html.twig', [
             'type' => $type,
+            'currentPage' => $currentPage,
         ]);
+    }
+
+    private function getVisiblePages(Pagerfanta $pager): array
+    {
+        $currentPage = $pager->getCurrentPage();
+
+        $nbPages = $pager->getNbPages();
+
+        $pages = [];
+
+        // Toujours afficher la première page
+        $pages[] = 1;
+
+        // Pages autour de la page actuelle (3 avant et 3 après)
+        for ($i = max(2, $currentPage - 3); $i <= min($nbPages - 1, $currentPage + 3); ++$i) {
+            $pages[] = $i;
+        }
+
+        // Toujours afficher la dernière page
+        if ($nbPages > 1) {
+            $pages[] = $nbPages;
+        }
+
+        // Éliminer les doublons et maintenir l'ordre
+        return array_values(array_unique($pages));
+    }
+
+    private function getCurrentPage(): int
+    {
+        $session = $this->requestStack->getSession();
+        return $session->get('current_page', 1);
     }
 }
