a8c3b75b80e648ef1cb65e8d47bfaa173e95142c
Quartz-42 <bbaroche42@gmail.com>
2026-01-10 12:02:02 +0100

    Run phpcs/stan

--- src/Repository/PokemonResistanceRepository.php
+++ src/Repository/PokemonResistanceRepository.php
@@ -9,10 +9,6 @@ use Doctrine\Persistence\ManagerRegistry;
 /**
  * @extends ServiceEntityRepository<PokemonResistance>
  *
- * @method PokemonResistance|null find($id, $lockMode = null, $lockVersion = null)
- * @method PokemonResistance|null findOneBy(array $criteria, array $orderBy = null)
- * @method PokemonResistance[]    findAll()
- * @method PokemonResistance[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 class PokemonResistanceRepository extends ServiceEntityRepository
 {
